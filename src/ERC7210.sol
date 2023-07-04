d3762c5491f8c358f0b68fdb4f6f1804b26d7731
Wilson Cusack <cusackw@gmail.com>
2023-07-04 11:14:49 -0400

    WIP'

--- src/ERC7210.sol
+++ src/ERC7210.sol
@@ -1,7 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-contract Counter {
+import {ERC721} from "solady/src/tokens/ERC721.sol";
+
+abstract contract ERC721O is ERC721 {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
